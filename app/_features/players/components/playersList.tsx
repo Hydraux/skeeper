5d0d0059df8aad722fd8b5c92a5a07f5cb8a3ba1
Ryan Bezold <ryan.bezold@gmail.com>
2025-01-11 02:16:06 +0000

    chore: Fix formatting

--- app/_features/players/components/playersList.tsx
+++ app/_features/players/components/playersList.tsx
@@ -3,102 +3,113 @@
 import { Player } from "@prisma/client";
 import PlayerCard from "./playerCard";
 import { useEffect, useOptimistic, useTransition } from "react";
-import { AppShell, Stack } from "@mantine/core";
+import { AppShell, Modal, Stack } from "@mantine/core";
 import AppHeader from "@/app/_components/appHeader";
 import { v4 as uuidv4 } from 'uuid';
 import { CreatePlayer, OnMessage, RemovePlayer } from "../actions";
 import { toast } from "react-toastify";
 import { socket } from "@/app/socket";
 import { PlayersReducerAction } from "../types";
+import { useDisclosure } from "@mantine/hooks";
+import PlayerForm from "./playerForm";
 
 interface Props {
-    players: Player[]
+  players: Player[];
+  roomId: string;
+  roomName: string;
 }
 
-export default function PlayersPage({ players }: Props) {
-    const [_, startTransition] = useTransition(); // eslint-disable-line @typescript-eslint/no-unused-vars
+export default function PlayersPage({ players, roomId, roomName }: Props) {
+  const [opened, { open, close }] = useDisclosure(false);
 
-    const [optimisticPlayers, modifyOptimisticPlayers] = useOptimistic(players, (state, action: PlayersReducerAction) => {
-        switch (action.type) {
-            case "Add":
-                return [...state, action.payload]
-            case "Remove":
-                return state.filter(player => player.id !== action.payload.id)
-            default:
-                return state;
-        }
-    });
+  const [_, startTransition] = useTransition(); // eslint-disable-line @typescript-eslint/no-unused-vars
 
-    useEffect(() => {
-        if (socket.connected) {
-            onConnect();
-        }
+  const [optimisticPlayers, modifyOptimisticPlayers] = useOptimistic(players, (state, action: PlayersReducerAction) => {
+    switch (action.type) {
+      case "Add":
+        return [...state, action.payload]
+      case "Remove":
+        return state.filter(player => player.id !== action.payload.id)
+      default:
+        return state;
+    }
+  });
 
-        function onConnect() {
-        }
-
-        function onDisconnect() {
-        }
-
-        function onMessage() {
-            OnMessage();
-
-        }
-
-        socket.on("connect", onConnect);
-        socket.on("disconnect", onDisconnect);
-        socket.on("message", onMessage);
-
-        return () => {
-            socket.off("connect", onConnect);
-            socket.off("disconnect", onDisconnect);
-        };
-    }, []);
-
-    const handleCreate = async (formData: FormData) => {
-        const name = formData.get("name");
-        if (!name) throw new Error("Invalid form data");
-        const player: Player = {
-            id: uuidv4(),
-            dateCreated: new Date(Date.now()),
-            name: name.toString(),
-            score: 0,
-        }
-        modifyOptimisticPlayers({ type: "Add", payload: player });
-        const result = await CreatePlayer(player);
-
-        if (result?.error) {
-            toast.error(result.error);
-            return false;
-        }
-        socket.emit("message", "Player added");
-        return true;
+  useEffect(() => {
+    if (socket.connected) {
+      onConnect();
     }
 
-    const handleDelete = async (player: Player) => {
-        modifyOptimisticPlayers({ type: "Remove", payload: player });
-        const result = await RemovePlayer(player);
-
-        if (result?.error) {
-            toast.error(result.error);
-            return;
-        }
-        socket.emit("message", "Player removed");
-        return;
+    function onConnect() {
     }
 
-    return (
-        <>
-            <AppShell.Header>
-                <AppHeader title="Game 1" onCreate={handleCreate} />
-            </AppShell.Header>
-            <AppShell.Main>
-                <Stack gap={10}>
-                    {optimisticPlayers.sort((a, b) => a.dateCreated.getTime() - b.dateCreated.getTime()).map(player =>
-                        <PlayerCard key={player.id} player={player} onDelete={player => startTransition(() => handleDelete(player))} />
-                    )}
-                </Stack>
-            </AppShell.Main>
-        </>
-    );
+    function onDisconnect() {
+    }
+
+    async function onMessage() {
+      await OnMessage();
+
+    }
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+    socket.on("message", onMessage);
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+    };
+  }, []);
+
+  const handleCreate = async (formData: FormData) => {
+    const name = formData.get("name");
+    if (!name) throw new Error("Invalid form data");
+    const player: Player = {
+      id: uuidv4(),
+      dateCreated: new Date(Date.now()),
+      name: name.toString(),
+      score: 0,
+      roomId: roomId,
+    }
+    modifyOptimisticPlayers({ type: "Add", payload: player });
+    const result = await CreatePlayer(player);
+
+    if (result?.error) {
+      toast.error(result.error);
+      return false;
+    }
+    socket.emit("message", "Player added");
+    close();
+    return true;
+  }
+
+  const handleDelete = async (player: Player) => {
+    modifyOptimisticPlayers({ type: "Remove", payload: player });
+    const result = await RemovePlayer(player);
+
+    if (result?.error) {
+      toast.error(result.error);
+      return;
+    }
+    socket.emit("message", "Player removed");
+    return;
+  }
+
+  return (
+    <>
+      <AppShell.Header>
+        <AppHeader title={roomName} open={open} withBackButton />
+        <Modal opened={opened} withCloseButton onClose={close} title="Create Player">
+          <PlayerForm onSubmit={handleCreate} />
+        </Modal>
+      </AppShell.Header>
+      <AppShell.Main>
+        <Stack gap={10}>
+          {optimisticPlayers.sort((a, b) => a.dateCreated.getTime() - b.dateCreated.getTime()).map(player =>
+            <PlayerCard key={player.id + player.score} player={player} onDelete={player => startTransition(() => handleDelete(player))} />
+          )}
+        </Stack>
+      </AppShell.Main>
+    </>
+  );
 }
