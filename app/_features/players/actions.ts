27705b76c5addfaaaf913c761fa37df7b30dc15b
Ryan Bezold <ryan.bezold@gmail.com>
2025-01-11 02:13:39 +0000

    fix: Apply score modifications accurately

--- app/_features/players/actions.ts
+++ app/_features/players/actions.ts
@@ -5,45 +5,35 @@ import { Player } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 
 export async function CreatePlayer(player: Player) {
-    try {
-        await prisma.player.create({ data: player });
-    } catch (error: any) { // eslint-disable-line @typescript-eslint/no-explicit-any
-        return { error: error.message || "Failed to create player"}
-    } finally {
-        revalidatePath("/");
-    }
+  try {
+    await prisma.player.create({ data: player });
+  } catch (error: any) { // eslint-disable-line @typescript-eslint/no-explicit-any
+    return { error: error.message || "Failed to create player" }
+  } finally {
+    revalidatePath("/");
+  }
 }
 
 export async function RemovePlayer(player: Player) {
-    try {
-        await prisma.player.delete({ where: {id: player.id}});
-    } catch (error: any) { // eslint-disable-line @typescript-eslint/no-explicit-any
-        return { error: error.message || "Failed to delete player"}
-    } finally {
-        revalidatePath("/");
-    }
-}
-
-export async function IncrementScore(player: Player, amount: number){
-    try {
-        await prisma.player.update({where: {id: player.id}, data: {score: {increment: amount}}})
-    } catch (error: any) { // eslint-disable-line @typescript-eslint/no-explicit-any
-        return { error: error.message || "Failed to increment player score"}
-    } finally {
-        revalidatePath("/");
-    }
-}
-
-export async function DecrementScore(player: Player, amount: number){
-    try {
-        await prisma.player.update({where: {id: player.id}, data: {score: {decrement: amount}}})
-    } catch (error: any) { // eslint-disable-line @typescript-eslint/no-explicit-any
-        return { error: error.message || "Failed to decrement player score"}
-    } finally {
-        revalidatePath("/");
-    }
-}
-
-export async function OnMessage(){
+  try {
+    await prisma.player.delete({ where: { id: player.id } });
+  } catch (error: any) { // eslint-disable-line @typescript-eslint/no-explicit-any
+    return { error: error.message || "Failed to delete player" }
+  } finally {
     revalidatePath("/");
+  }
+}
+
+export async function SetScore(playerId: string, score: number) {
+  try {
+    await prisma.player.update({ where: { id: playerId }, data: { score: score } })
+  } catch (error: any) { // eslint-disable-line @typescript-eslint/no-explicit-any
+    return { error: error.message || "Failed to set player score" }
+  } finally {
+    revalidatePath("/");
+  }
+}
+
+export async function OnMessage() {
+  revalidatePath("/");
 }
